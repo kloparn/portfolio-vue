e24e9a684a9b00f07565f11934cdb05faf7c6101
kloparn <adam97.live.com@live.com>
2020-08-25 11:02:35 +0200

    using axios in the About page and the project page to get all the info from the backend, now going to try and fix bootstrap for the cards

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -1,20 +1,37 @@
 <template>
-  <div id="layout">Project Page</div>
+  <div id="layout">
+    <div v-for="item in repos" :key="item.id">
+      {{ item.id }} {{ item.title }}
+      <br />
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Projects",
+  data() {
+    return {
+      repos: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://dotnet-core-backend.herokuapp.com/api/Github")
+      .then((Response) => (this.repos = Response.data));
+  },
 };
 </script>
 
 <style scoped>
 #layout {
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
-  text-align: center;
-  background-size: cover;
-  color: whitevh;
+  align-items: center;
+  color: white;
   min-height: 100vh;
   text-shadow: 2px 2px black;
 }
